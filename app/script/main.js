8f7e33c90061cb251ca3237a0cd4423682f85d57
Михаил Сапицин <sapicin@texterra.local>
2019-08-16 18:45:52 +0300

    wip

--- app/script/main.js
+++ app/script/main.js
@@ -8,17 +8,59 @@ function close(){
   $('.footer__hidden').children().fadeOut();
 }
 
+$(document).ready(function(){
+  if($(window).width() < 768) {
+    $('.hotels__item').addClass('active')
+  }
+});
+
+$(window).resize(function (){
+  if($(window).width() < 768) {
+    $('.hotels__item').addClass('active')
+  } else {
+    $('.hotels__item').not('.hotels__item:first').removeClass('active')
+  }
+});
+
 function change(e){
+  if($(window).width() > 768) {
   $(e).addClass('active');
   $(e).siblings().removeClass('active');
-  console.log('123')
+  }
 }
 
-$('[close-button]').click(function(){
+function callback(){
+  $('body').addClass('body-fixed')
+  $('.footer__hidden').css('display', 'flex')
+}
+
+
+$(document).scroll(function(){
+  var topWindow = $(window).scrollTop();
+  if ( topWindow >= 200 ) {
+      $('.button-top').fadeIn(300);
+  } else {
+      $('.button-top').fadeOut(300);
+  }
+});
+
+
+$('[close-button]').on('click', function(){
   close();
 })
 
+$('.button-top').on('click', function(){
+  $("html").animate({ scrollTop: 0 }, "slow");
+});
 
-$('.hotels__item').click(function(){
+
+
+$('.hotels__item').on('click', function(){
   change(this);
 })
+
+$(document).ready(function(){
+  if($(window).width() < 768) {
+    $('.hotels__item').addClass('active')
+  }
+});
